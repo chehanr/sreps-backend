b231e6cfac88d870c2090330e68dcf2d75b804c3
chehanr <chehan.rat@gmail.com>
2019-10-10 22:22:28 +1100

    Implemented invoice logic/ changed serializers.

--- sreps/api/v1/views/invoice.py
+++ sreps/api/v1/views/invoice.py
@@ -1,9 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, mixins, viewsets
+from rest_framework import exceptions, filters, mixins, viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
-from sreps.api.v1.serializers.invoice import InvoiceSerializer
-from sreps.core.models import Invoice
+from rest_framework.response import Response
+from sreps.api.v1.serializers.invoice import InvoiceListSerializer, InvoiceDetailSerializer, InvoiceCreateSerializer
+from sreps.core.models import Invoice, Sale, Product
+from django.core.exceptions import ValidationError
 
 
 class InvoiceViewSet(
@@ -15,7 +17,6 @@ class InvoiceViewSet(
         viewsets.GenericViewSet,):
 
     queryset = Invoice.objects.all()
-    serializer_class = InvoiceSerializer
     permission_classes = (IsAdminUser,)
     filter_backends = (
         filters.SearchFilter,
@@ -45,3 +46,74 @@ class InvoiceViewSet(
         'customer__id',
         'is_paid',
     )
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return InvoiceListSerializer
+        if self.action == 'create':
+            return InvoiceCreateSerializer
+
+        return InvoiceDetailSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = InvoiceCreateSerializer(
+            context={'request': request}, data=request.data)
+
+        if not serializer.is_valid():
+            raise exceptions.ValidationError(serializer.errors)
+
+        products = request.data.get('products')
+
+        if not products:
+            raise exceptions.ValidationError({"products": "Products cannot be empty"})
+
+        invoice_obj = serializer.save()
+
+        sale_objs = []
+        product_errors = []
+
+        # Validation
+        for product in products:
+            product_id = product.get('id')
+            product_quantity = product.get('quantity')
+
+            if not Product.objects.filter(pk=product_id).exists():
+                product_errors.append({
+                    product_id: 'Product doesn\'t exist'
+                })
+            if not product_quantity:
+                product_errors.append({
+                    product_id: 'Product quantity doesn\'t exist'
+                })
+
+        if product_errors:
+            product_error_dict = {"products": product_errors}
+            raise exceptions.ValidationError(product_error_dict)
+
+        sales_errors = {}
+
+        for product in products:
+            product_id = product['id']
+            product_quantity = product['quantity']
+
+            product_obj = Product.objects.get(pk=product_id)
+
+            sale_obj = None
+
+            try:
+                sale_obj = Sale(invoice=invoice_obj, product=product_obj,
+                                quantity=product_quantity).save()
+            except ValidationError as ve:
+                sales_errors['ve'] = ve
+
+            sale_objs.append(sale_obj)
+
+        if sales_errors.get('ve'):
+            invoice_obj.delete()
+            for sale_obj in sale_objs:
+                if sale_obj != None:
+                    sale_obj.delete()
+
+            return exceptions.ValidationError(sales_errors['ve'])
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
