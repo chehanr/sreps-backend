5c19a826de28872a70fd34d0aa541fb4aa74f1b1
chehanr <chehan.rat@gmail.com>
2019-10-16 11:10:36 +1100

    Added csv export to sales.

--- sreps/api/v1/views/sale.py
+++ sreps/api/v1/views/sale.py
@@ -1,8 +1,10 @@
-from django_filters.rest_framework import DjangoFilterBackend # pylint:disable=unresolved-import
+from django_filters.rest_framework import DjangoFilterBackend  # pylint:disable=unresolved-import
 from rest_framework import filters, mixins, viewsets
-from rest_framework.decorators import action
+from rest_framework.decorators import action, renderer_classes
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from sreps.api.v1.serializers.sale import SaleSerializer
+from sreps.api.v1.renderers.sale import SaleCsvRenderer
 from sreps.core.models import Sale
 
 
@@ -35,3 +37,38 @@ class SaleViewSet(
         'invoice__id',
         'product__id',
     )
+
+    @action(detail=False, methods=['GET'], name='Export', renderer_classes=[SaleCsvRenderer])
+    def export(self, request, pk=None):
+        """Get CSV export for sales."""
+
+        sale_objs = Sale.objects.all()
+        content = [
+            {
+                'id': sale_obj.pk,
+                'quantity': sale_obj.quantity,
+                'datetime-created': sale_obj.datetime_created,
+                'invoice-id': sale_obj.invoice.pk,
+                'invoice-description': sale_obj.invoice.description,
+                'invoice-other-cost': sale_obj.invoice.other_cost,
+                'invoice-tax-amount': sale_obj.invoice.tax_amount,
+                'invoice-is-paid': sale_obj.invoice.is_paid,
+                'invoice-datetime-pay-due': sale_obj.invoice.datetime_pay_due,
+                'invoice-datetime-paid': sale_obj.invoice.datetime_paid,
+                'invoice-datetime-created': sale_obj.invoice.datetime_created,
+                'invoice-salesperson-id': sale_obj.invoice.salesperson.pk,
+                'invoice-salesperson-username': sale_obj.invoice.salesperson.username,
+                'invoice-customer-id': sale_obj.invoice.customer.pk,
+                'invoice-customer-name': sale_obj.invoice.customer.name,
+                'product-id': sale_obj.product.pk,
+                'product-name': sale_obj.product.name,
+                'product-category': sale_obj.product.category,
+                'product-lst': sale_obj.product.low_stock_threshold,
+                'product-base-price': sale_obj.product.base_price,
+                'product-discount-amount': sale_obj.product.discount_amount,
+                'product-datetime-created': sale_obj.product.datetime_created,
+            }
+            for sale_obj in sale_objs
+        ]
+
+        return Response(content)
